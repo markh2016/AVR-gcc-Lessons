cbe57cd28c97025169dcd7a9155597d1eb468a91
Mark Harrington <markh2016@users.noreply.github.com>
2024-02-04 01:30:12 +0000

    Update main.c

--- Lesson1/C_Files/main.c
+++ Lesson1/C_Files/main.c
@@ -1,12 +1,12 @@
 #include "lesson1.h"
 
 #define F_CPU 16000000
-#define LED PD4  // Change LED to be PB0
+#define LED PD4  // Change LED to be PD4
 
 int main(void) {
     DDRD |= (1 << LED);  // Configure PORTB0 (PB0) as output instead of PORTD4
     
-    PORTD |= (1 << LED); // Set PB0 high (LED on)
+    PORTD |= (1 << LED); // Set PD4 high (LED on)
 
     while(1) {
         // Your code here (this loop will run indefinitely)
