d867f591d1df50bdb1372f335f118d200d9f988e
Mark Harrington <markh2016@users.noreply.github.com>
2024-02-04 01:31:37 +0000

    Update main.c

--- Lesson1/C_Files/main.c
+++ Lesson1/C_Files/main.c
@@ -4,7 +4,7 @@
 #define LED PD4  // Change LED to be PD4
 
 int main(void) {
-    DDRD |= (1 << LED);  // Configure PORTB0 (PB0) as output instead of PORTD4
+    DDRD |= (1 << LED);  // Configure PORTB0 (PD4) as output 
     
     PORTD |= (1 << LED); // Set PD4 high (LED on)
 
